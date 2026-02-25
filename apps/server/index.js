5b14222e0b36a5d3ff4eb4267178a126d93f6de2
Moayad <moayadali.g@gmail.com>
2026-02-25 23:22:44 +0200

    remove console logs

--- apps/server/index.js
+++ apps/server/index.js
@@ -20,7 +20,6 @@ app.use(express.json());
 
 app.post("/api/boards/create", (req,res) => {
     const canvas = req.body;
-    console.log("request made to create board");
     const makeId = (n = 6) => {
         let s = "";
         while (s.length < n) s += Math.random().toString(36).slice(2);
@@ -28,7 +27,6 @@ app.post("/api/boards/create", (req,res) => {
     };
     const boardId = makeId(6);
     boards[boardId] = canvas
-    console.log(boards);
     res.json({ id: boardId })
 })
 
@@ -39,14 +37,11 @@ app.get("/api/boards/:boardId", (req, res) => {
 
 
 io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
     socket.on("add-stroke", (data) => {
         const { stroke, boardId } = data;
         if (!boards[boardId]) {
-            console.error("Invalid board ID:", boardId);
             return;
         }
-        console.log("received stroke", stroke.id);
         boards[boardId].strokes[stroke.id] = stroke;
         socket.to(boardId).emit("set-strokes", boards[boardId].strokes);
     });
@@ -54,12 +49,9 @@ io.on("connection", (socket) => {
     socket.on("stroke-batch", (data) => {
         const { points, strokeId, strokeMeta, boardId } = data;
         if (!points || !strokeId || !boardId) {
-            console.error("Invalid batch data:", data);
             return;
         }
-        console.log("received batch for stroke", strokeId, "with", points.length, "points");
         if (!boards[boardId]) {
-            console.error("Invalid board ID:", boardId);
             return;
         }
         if (!boards[boardId].strokes[strokeId]) {
@@ -76,10 +68,8 @@ io.on("connection", (socket) => {
     socket.on("delete-stroke", (data) => {
         const { id, boardId } = data;
         if (!boards[boardId]) {
-            console.error("Invalid board ID:", boardId);
             return;
         }
-        console.log("deleting stroke", id);
         delete boards[boardId].strokes[id];
         socket.to(boardId).emit("set-strokes", boards[boardId].strokes);
     });
@@ -95,5 +85,4 @@ io.on("connection", (socket) => {
 });
 
 httpServer.listen(3000, () => {
-    console.log("Server is listening on port 3000");
 });
